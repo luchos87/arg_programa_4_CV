caa69cca69dd7de4a98c38381eea0bff11300ca3
luchos <luch05_77@hotmail.com>
2023-01-13 18:49:51 -0300

    usando la API

--- JS/js.js
+++ JS/js.js
@@ -1,3 +1,17 @@
+let obj;
 fetch('https://randomuser.me/api/')
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+        obj = json;
+    })
+    .then(() => {
+        console.log(obj);
+        document.getElementById("imagenPerfil").src = obj.results[0].picture.medium;
+    });
+
+
+
+    // const obj = JSON.parse(json);
+    // console.log(obj);
+    // document.getElementById("imagenPerfil").src = obj.results.picture.medium;
+1
