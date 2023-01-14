ac52724e36aae4a7634aead4d2ca5fc4007348fe
luchos <luch05_77@hotmail.com>
2023-01-13 21:13:08 -0300

    header responsive

--- JS/js.js
+++ JS/js.js
@@ -6,12 +6,12 @@ fetch('https://randomuser.me/api/')
     })
     .then(() => {
         console.log(obj);
-        document.getElementById("imagenPerfil").src = obj.results[0].picture.medium;
+        document.getElementById("imagenPerfil").src = obj.results[0].picture.large;
+        document.getElementById("nombreCompleto").innerHTML = obj.results[0].name.first + " " +  obj.results[0].name.last;
+        document.getElementById("edad").innerHTML = obj.results[0].dob.age + " Años";
+        document.getElementById("telefono").innerHTML = obj.results[0].cell;
+        document.getElementById("direccion").innerHTML = obj.results[0].location.street.name + " " +  obj.results[0].location.street.number + ", " + obj.results[0].location.city + " " + obj.results[0].location.country;
+        document.getElementById("email").innerHTML = obj.results[0].email;
     });
 
-
-
-    // const obj = JSON.parse(json);
-    // console.log(obj);
-    // document.getElementById("imagenPerfil").src = obj.results.picture.medium;
 1
